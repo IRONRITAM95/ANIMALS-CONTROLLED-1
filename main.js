b98f29e8dfd49bd8ddc36f446651a8c54fa05c3d
IRONRITAM95 <78020914+IRONRITAM95@users.noreply.github.com>
2021-09-29 16:10:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function hearing(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classify = ml5.soundClassifier("https://storage.googleapis.com/tm-model/40fTo3U4-/model.json", modelEngage);
+}
+
+function modelEngage(){
+    classify.classify(confidence);
+}
+
+function confidence(error, results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results)
+    r = Math.floor(Math.random()* 255)+1 ; 
+    g = Math.floor(Math.random()* 255)+1 ;
+    b = Math.floor(Math.random()* 255)+1 ;
+
+  document.getElementById("noize").innerHTML= results[0].label ;
+  document.getElementById("percent").innerHTML= (results[0].confidence*100).toFixed(2) +"%" ;
+
+  document.getElementById("noize").style.color= "rgb("+r+","+g+","+b+")" ;
+  document.getElementById("percent").style.color= "rgb("+r+","+g+","+b+")" ;
+}
+}
